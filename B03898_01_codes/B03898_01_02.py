ab12d72193c72ccee319ad1c5a143c9b60df0b37
zhongfaliao <38413816+zhongfaliao@users.noreply.github.com>
2019-07-16 09:21:43 -0400

    Update B03898_01_02.py

--- B03898_01_codes/B03898_01_02.py
+++ B03898_01_codes/B03898_01_02.py
@@ -6,7 +6,7 @@ This file contains Python codes.
 """
 from functools import partial
 def greeting(my_greeting, name):
-    print "%s %s" % (my_greeting, name)
+    print( "%s %s" % (my_greeting, name))
 
 say_hello_to = partial(greeting, "Hello")
 say_hello_to("World")
