45cdc5d73147b8c850225ff23926e256e6dbd98b
zhongfaliao <38413816+zhongfaliao@users.noreply.github.com>
2019-07-22 10:55:23 -0400

    Update B03898_02_01.py

--- B03898_02_Codes/B03898_02_01.py
+++ B03898_02_Codes/B03898_02_01.py
@@ -13,13 +13,13 @@ mkt_returns = [0.055, -0.09, -0.041, 0.045, 0.022]
 
 beta, alpha, r_value, p_value, std_err = \
     stats.linregress(stock_returns, mkt_returns)
-print beta, alpha
+print (beta, alpha)
 
 """ Calculating the SML """
 rf = 0.05
 mrisk_prem = 0.085
 risk_prem = mrisk_prem * beta
-print "Risk premium:", risk_prem
+print ("Risk premium:", risk_prem)
 
 expected_stock_return = rf + risk_prem
-print "Expected stock return:", expected_stock_return
+print ("Expected stock return:", expected_stock_return)
